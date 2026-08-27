858c68dda8620984666da838d344ccd82ea50d6f
Sourav kumar <souravkumar4374@gmail.com>
2026-08-27 19:06:32 +0530

    Time: 48 ms (52.46%) | Memory: 29.3 MB (28.45%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -13,21 +13,24 @@ public:
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum == 0) {
                     ans.push_back({nums[i], nums[left], nums[right]});
-                    left++;
-                    right--;
-                    while(left < right && nums[left] == nums[left - 1])
+                    while(left < right && nums[left] == nums[left + 1])
                         left++;
-
-                    while(left < right && nums[right] == nums[right + 1])
+                    while(left < right && nums[right] == nums[right - 1])
                         right--;
+                    left++;
+
+                    right--;
                 }
                 else if(sum < 0) {
                     left++;
                 }
                 else {
                     right--;
+
                 }
             }
+
+
         }
         return ans;
     }
