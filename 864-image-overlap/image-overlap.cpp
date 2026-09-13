302aeec99cbfc5be542893a8f49b6d58d96579e5
Sourav kumar <souravkumar4374@gmail.com>
2026-09-13 11:58:01 +0530

    Time: 7 ms (96.64%) | Memory: 14 MB (47.76%) - LeetSync

--- 864-image-overlap/image-overlap.cpp
+++ 864-image-overlap/image-overlap.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2){
-        int n = img1.size();
-        int ans = 0;
-        for(int dr = -n + 1; dr < n; dr++){
-            for(int dc= -n + 1; dc < n; dc++){
-                int count= 0;
-                for(int i= 0; i < n; i++){
-                    for(int j= 0; j < n; j++){
-                        int ni = i + dr;
-                        int nj = j + dc;
-                        if(ni >= 0 && ni < n && nj >=0 && nj< n){
-                            if(img1[i][j] == 1 && img2[ni][nj] == 1){
-                                count++;
-                            }
-                        }
-                    }
-                }
-
-                ans = max(ans,count);
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n =img1.size();
+        vector<pair<int,int>> v1,v2;        
+        for (int i =0;i<n;i++){
+            for (int j =0;j<n;j++) {
+                if (img1[i][j] == 1) v1.push_back({i,j});
+                if (img2[i][j] == 1) v2.push_back({i,j});
             }
-        }
+        }      
+        int ans =0;
+        vector<vector<int>>count(2*n+1,vector<int>(2*n+1,0));       
+        for (auto& p1 :v1){
+            for (auto& p2 :v2){
+                int dx = p1.first - p2.first + n;
+                int dy = p1.second - p2.second + n;
+                count[dx][dy]++;
+                ans = max(ans,count[dx][dy]);
+            }
+        }       
         return ans;
     }
 };
